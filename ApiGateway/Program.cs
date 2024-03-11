f99ef7fef0d7b3fac841d99be6f3b070d2206c43
Philippe CHARLES <A114SR@login.axa>
2024-03-11 15:09:58 +0100

    mise en place de l passerelle ApigateWay

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,46 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
+namespace OcelotBasic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile("ocelot.json")
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s => {
+                s.AddOcelot();
+                // Configure your logging here
+                s.AddLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole(); // Add console logging, you can add other providers as needed
+                                          // Add other logging providers here
+                });
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                //add your logging
+            })
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.UseOcelot().Wait();
+            })
+            .Build()
+            .Run();
+        }
+    }
+}
